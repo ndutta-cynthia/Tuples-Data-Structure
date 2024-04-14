7fb5fdafacb2080b74540a5cd5474955d5f0239b
ndutta-cynthia <nduttamaina@gmail.com>
2024-04-14 18:12:06 +0300

    added my tuples assignment

---
+++ tuples.py
@@ -0,0 +1,24 @@
+# 1. Creating a tuple
+names= ( 'Mary','Martin', 'Jackline', 'James')
+print(names)
+        
+        # Accessing an elent in a tuple
+print(names[2])
+
+# Finding the length of a tuple
+print('Total Item:', len(names))
+
+# Iterating through a tuple
+for name in names: 
+    print(name)
+
+# To check if item exists in a tuple
+print('Martin' in names) 
+print('Caroline' in names) 
+
+# Creating a tuple with one item
+
+greeting = ('Hello') 
+print(greeting)
+
+        
